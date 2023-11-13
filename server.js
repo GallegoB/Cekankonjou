c6fe0fad6dcc1bd91d03c382228010c0a628a4e6
Gallego Baptiste <bapgallego@gmail.com>
2023-11-13 15:39:58 +0100

    Configuration back-end sur un fichier Json

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require("express");
-require("dovenv").config();
+require("dotenv").config();
 
 const app = express();
 
-const port = process.env.port || 3000;
+const games = require("./bd.json");
+
+const port = process.env.port || 7000;
+
+app.use(express.json());
+// lecture complete json
+app.get("/api/games", async (req, res) => {
+  res.json(games);
+});
+// Lecture d'un par l'id
+app.get("/api/games/:gameId", async (req, res) => {
+  const id = req.params.gameId;
+  const game = games.find((game) => game.id === id);
+
+  if (!game) {
+    return res.status(404).send("Jeux non trover");
+  }
+  res.json(game);
+});
 
 app.listen(port, () => {
   console.log(`Le serveur est lancé sur le port : ${port}`);
