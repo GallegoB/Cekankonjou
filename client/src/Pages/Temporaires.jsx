ee75e485e19b62a450e7ffa47fb1ae4c480272f1
Gallego Baptiste <bapgallego@gmail.com>
2024-03-10 14:35:36 +0100

    Ajout text page temporaire

--- client/src/Pages/Temporaires.jsx
+++ client/src/Pages/Temporaires.jsx
@@ -4,6 +4,61 @@ function Temporaires(props) {
   return (
     <div>
       <h1>Les Ludothèques temporaires</h1>
+
+      <div class="entry-content">
+        <p>
+          Vous ne pouvez pas venir à la ludothèque alors nous venons à vous une
+          fois par mois !
+        </p>
+
+        <p>
+          Un temp de jeu sur place gratuit et un prêt de jeu pour continuer à
+          jouer à la maison !
+        </p>
+
+        <ul>
+          <li>
+            Le premier mardi de chaque mois à <strong>Yssingeaux</strong>, de
+            16h30 à 18h au Teen&#8217;s Club, place Maréchal Foch.
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            Le deuxième jeudi de chaque mois à <strong>Lantriac</strong>, de
+            16h30 à 18h à la bibliothèque.
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            Le deuxième samedi de chaque de 10h à 11h30 à{" "}
+            <strong>St Julien Chapteuil</strong>, à l&#8217;Echappée.
+          </li>
+        </ul>
+
+        <p></p>
+
+        <p></p>
+
+        <p></p>
+
+        <figure class="wp-block-image size-large">
+          <a href="https://www.Cekankonjou.fr/wp-content/uploads/2023/12/sit.png">
+            <img
+              fetchpriority="high"
+              decoding="async"
+              width="1024"
+              height="509"
+              src="https://www.Cekankonjou.fr/wp-content/uploads/2023/12/sit-1024x509.png"
+              alt=""
+              class="wp-image-2177"
+              srcset="https://www.Cekankonjou.fr/wp-content/uploads/2023/12/sit-1024x509.png 1024w, https://www.Cekankonjou.fr/wp-content/uploads/2023/12/sit-300x149.png 300w, https://www.Cekankonjou.fr/wp-content/uploads/2023/12/sit-768x382.png 768w, https://www.Cekankonjou.fr/wp-content/uploads/2023/12/sit.png 1433w"
+              sizes="(max-width: 1024px) 100vw, 1024px"
+            />
+          </a>
+        </figure>
+      </div>
     </div>
   );
 }
